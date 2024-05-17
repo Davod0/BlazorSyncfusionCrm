4a324a84145021ff3ed6ef80dc15581bf3074ba5
Davod <davodnikoyi@gmail.com>
2024-05-17 21:55:53 +0200

    Created a new folder named Data for making the classes needed for the database.

---
+++ BlazorSyncfusionCrm/Server/Data/DataContext.cs
@@ -0,0 +1,61 @@
+﻿using BlazorSyncfusionCrm.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorSyncfusionCrm.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=blazingcrm;Trusted_Connection=true;TrustServerCertificate=true");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Contact>().HasData(
+
+                new Contact
+                {
+                    Id = 1,
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    NickName = "Spider-man",
+                    DateOfBirth = new DateTime(2001, 8, 1),
+                    Place = "New York City"
+                },
+                new Contact
+                {
+                    Id = 2,
+                    FirstName = "Tony",
+                    LastName = "Stark",
+                    NickName = "Iron-man",
+                    DateOfBirth = new DateTime(1990, 8, 1),
+                    Place = "Chicago"
+                },
+              new Contact
+              {
+                  Id = 3,
+                  FirstName = "Bruce",
+                  LastName = "Banner",
+                  NickName = "Hulk",
+                  DateOfBirth = new DateTime(1990, 8, 1),
+                  Place = "Ohio"
+              }
+                );
+
+            modelBuilder.Entity<Note>().HasData(
+                new Note { Id = 1, ContactId = 1, Text = "With great power comes great responsibility"},
+                new Note { Id = 2, ContactId = 2, Text = "The magic you are searching for is in the work you avoiding"},
+                new Note { Id = 3, ContactId = 3, Text = "do not care about people"}
+                );
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Note> Notes { get; set; }
+    }
+}
