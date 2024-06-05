6f4ecad4c057dc99d2078f1dbd27c50a271643d0
Davod <davodnikoyi@gmail.com>
2024-06-05 20:44:24 +0200

    Implemented a new Note Controller (NotesController),  a new interface ICrudService and a new service class CrudService

---
+++ BlazorSyncfusionCrm/Server/Interfaces/ICrudService.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorSyncfusionCrm.Server.Interfaces
+{
+    public interface ICrudService<T> where T : class
+    {
+        Task<List<T>> GetAllAsync(); 
+        Task<T> GetByIdAsync(int id);
+        Task<T> AddAsync(T item);
+        Task<T> UpdateAsync(T item);
+        Task<T> DeleteAsync(int id);
+    }
+}
---
+++ BlazorSyncfusionCrm/Server/Services/CrudService.cs
@@ -0,0 +1,23 @@
+﻿using BlazorSyncfusionCrm.Server.Data;
+using BlazorSyncfusionCrm.Server.Interfaces;
+using BlazorSyncfusionCrm.Server.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace BlazorSyncfusionCrm.Server.Services
+{
+    public class CrudService<T> : ICrudService<T> where T : class
+    {
+        private readonly DataContext _context;
+        private readonly DbSet<T> _dbSet;
+        public CrudService(DataContext context) 
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+
+    }
+}
+
+
